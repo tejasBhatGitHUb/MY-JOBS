575259298c72406f82cc1d9b80d13d12cad80618
tejasBhatGitHUb <bgtejas2001@gmail.com>
2024-06-02 11:47:30 +0530

    Some changes have been made in the database.py file to add job application and posting confirmation. Additionally, it now checks if a job application or posting already exists in the database.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, jsonify, request
 
-from database import get_job, get_all_jobs,add_application_to_db,add_job,is_present
+from database import get_job, get_all_jobs, add_application_to_db, add_job, is_present
 
 app = Flask(__name__, template_folder="template")
 
@@ -11,44 +11,51 @@ def start():
     return render_template('home.html',
                            jobs=JOBS
                            )
+
+
 @app.route('/jobs/<id>')
 def display_job(id):
-    # try:
-    temp = get_job(id)
-    print(temp)
-    #     if not temp:
-    #         return render_template('ERROR_page.html')
-    return render_template('jobpage.html', job=temp[0])
-    # except:
-    #     return render_template('ERROR_page.html')
+    try:
+        temp = get_job(id)
+        return render_template('jobpage.html', job=temp[0])
+    except:
+        return render_template('ERROR_page.html', message1="Something went wrong!",
+                               message2="Encountered an unexpected ERROR while fetching your data.")
 
 
 @app.route('/jobs/<id>/apply')
 def apply_for_job(id):
-    # try:
-    return render_template('application_form.html', job=get_job(id)[0])
-    # except:
-    #     return render_template('ERROR_page.html')
+    try:
+        return render_template('application_form.html', job=get_job(id)[0])
+    except:
+        return render_template('ERROR_page.html')
 
 
-@app.route('/jobs/<id>/apply/submission',methods=['POST','GET'])
+@app.route('/jobs/<id>/apply/submission', methods=['POST', 'GET'])
 def form_submission(id):
     try:
-        application=request.form
-        if is_present(application['id']):
-            return render_template('ERROR_page.html',message1="Job Posting Status",message2="You have already posted this job.")
-        add_application_to_db(application,id)
+        application = request.form
+        print(add_application_to_db(application, id))
         return render_template('application_submitted.html', application=application)
     except:
-        return render_template('ERROR_page.html',message1="Something Went Wrong!",message2="We're sorry, but something went wrong while processing your request. Please try again later.")
+        return render_template('ERROR_page.html', message1="Something Went Wrong!",
+                               message2="We're sorry, but something went wrong while processing your request. Please try again later.")
 
-@app.route("/job-posting/submission",methods=["POST","GET"])
+
+@app.route("/job-posting/submission", methods=["POST", "GET"])
 def job_opening_submission():
-    application=request.form
+    application = request.form
+    if is_present(application['id'].strip()):
+        return render_template('ERROR_page.html', message1="Job Posting Status",
+                               message2="You have already posted this job.")
     add_job(application)
-    return render_template("job_posting_confirmation.html",application=application)
+    return render_template("job_posting_confirmation.html", application=application)
+
+
 @app.route('/job-posting')
 def post_a_job():
     return render_template('job_posting_application.html')
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=5000)
--- database.py
+++ database.py
@@ -14,26 +14,28 @@ cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 
 def add_job(application):
-    cursor.execute(f'''INSERT INTO jobs(id,role,location,company,website,salary,currency,responsibilities,requirements,experience,email)
-                VALUES('{application['id']}',
-                        '{application['role']}',
-                        '{application['location']}',
-                        '{application['company']}',
-                        '{application['website']}',
-                        {application['salary']},
-                        '{application['currency']}',
-                        '{application['responsibilities']}',
-                        '{application['requirements']}',
-                        '{application['experience']}',
-                        '{application['email']}');''')
-    connection.commit()
-
+    try:
+        cursor.execute(f'''INSERT INTO jobs(id,role,location,company,website,salary,currency,responsibilities,requirements,experience,email)
+                    VALUES('{application['id'].strip()}',
+                            '{application['role']}',
+                            '{application['location']}',
+                            '{application['company']}',
+                            '{application['website']}',
+                            {application['salary']},
+                            '{application['currency']}',
+                            '{application['responsibilities']}',
+                            '{application['requirements']}',
+                            '{application['experience']}',
+                            '{application['email']}');''')
+        connection.commit()
+        return True
+    except:
+        return False
 
 
 def get_job(id):
-    v=f'''SELECT id,role,location,company,website,salary,currency,responsibilities,requirements,created_at FROM jobs
+    v = f'''SELECT id,role,location,company,website,salary,currency,responsibilities,requirements,created_at FROM jobs
                WHERE id='{id}';'''
-    print(v)
     cursor.execute(v)
     x = cursor.fetchall()
     connection.commit()
@@ -41,30 +43,55 @@ def get_job(id):
 
 
 def get_all_jobs():
-    cursor.execute(f'''SELECT id,role,location,company,website,salary,currency,responsibilities,requirements,created_at FROM jobs
-                       ORDER BY created_at DESC''')
+    cursor.execute(f'''SELECT id,role,location,company,website,salary,currency,responsibilities,requirements,created_at FROM jobs;''')
     x = cursor.fetchall()
     connection.commit()
     return x
 
-def add_application_to_db(application,id):
+
+def delete_application(id,email,linkedin):
+    cursor.execute(f'''DELETE  FROM applications
+                           WHERE (id='{id}' AND (email='{email}' OR linkedin_url='{linkedin}'));
+                        ''')
+    connection.commit()
+
+
+def add_application_to_db(application, id):
     try:
+        name=application['name'].strip()
+        email=application['email'].strip()
+        linkedin=application['linkedin'].strip()
+        resume=application['resume'].strip()
+        if already_applied(id,email,linkedin):
+            delete_application(id,email,linkedin)
         cursor.execute(f'''INSERT INTO applications(id,full_name,email,highest_education,years_of_experience,skills,linkedin_url,resume_url)
                            VALUES('{id}',
-                                   '{application['name']}',
-                                   '{application['email']}',
+                                   '{name}',
+                                   '{email}',
                                    '{application['education']}',
                                    {application['experience']},
                                    '{application['skills']}',
-                                   '{application['linkedin']}',
-                                   '{application['resume']}')''')
+                                   '{linkedin}',
+                                   '{resume}')''')
         connection.commit()
         return True
-    except :
+    except:
+        connection.commit()
         return False
+
+
 def is_present(id):
     cursor.execute(f'''SELECT id FROM jobs
                        WHERE id='{id}';''')
-    x=cursor.fetchall()
+    x = cursor.fetchall()
     connection.commit()
     return True if x else False
+
+
+def already_applied(id,email,linkedin):
+    cursor.execute(f'''SELECT  id,applicant_id,email,linkedin_url FROM applications
+                       WHERE (id='{id}' AND (email='{email}' OR linkedin_url='{linkedin}'));
+                    ''')
+    t = cursor.fetchall()
+    connection.commit()
+    return True if t else False
